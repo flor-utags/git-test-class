352a90baae52c3c813227600f3548549cf6e3f04
Flor Rangel <flor.rangel1976@gmail.com>
2022-05-14 17:52:28 -0500

    feat: added the first

---
+++ index.js
@@ -0,0 +1,9 @@
+function doSomething() {
+    console.log("Hello Worlds");
+}
+function develop(){
+    console.log("Rama develop new");
+}
+function testing(){
+    console.log("Hello testinh");
+}
